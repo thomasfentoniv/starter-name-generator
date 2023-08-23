d8ac1767e4b21dfa70e1eca489e89a2c4924bf21
Thomas Fenton <thomasfenton@Thomass-Air.home>
2023-08-23 12:16:00 -0700

    make changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
   const [name, setName] = useState([])
 
-useEffect(() => {
-  fetch('https://starter-name-generator-ssg5.onrender.com/generate') // Change this URL to the one that you just deployed
-    .then((res) => res.json())
-    .then((name) => setName([name]))
-
+  useEffect(() => {
+    fetch('https://starter-name-generator-ssg5.onrender.com/generate')
+      .then((res) => res.json())
+      .then((nme) => setName([nme]))
+  }, [])
 
   return (
     <h1>{name}</h1>
