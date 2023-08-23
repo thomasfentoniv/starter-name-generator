8d7567505ad3c1376d98ec1f773b18dda318050d
Thomas Fenton <thomasfenton@Thomass-Air.home>
2023-08-23 08:48:51 -0700

    re-commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
   const [name, setName] = useState([])
 
-  useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
-      .then((res) => res.json())
-      .then((nme) => setName([nme]))
-  }, [])
+useEffect(() => {
+  fetch('https://starter-name-generator-render-8ebi.onrender.com/') // Change this URL to the one that you just deployed
+    .then((res) => res.json())
+    .then((name) => setName([name]))
+
 
   return (
     <h1>{name}</h1>
